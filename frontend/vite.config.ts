2f4348d22e1fe92af0601ae7cc2d9e99f23bed63
tarunkamra <tarunkamra18@gmail.com>
2024-09-06 22:29:08 +0530

    cicd

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: true, // Allow access from outside the container
+    port: 5173  // Ensure this port matches the one exposed in Docker
+  }
 })
